ee23fb3f41488955f222f00f956a3e7047e67b4b
Roberts Kursitis <53969619+Automata02@users.noreply.github.com>
2025-02-10 11:15:45 +0200

    updated sharships screen

--- views/mainViews/starshipView.tsx
+++ views/mainViews/starshipView.tsx
@@ -1,56 +1,25 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View, Text, FlatList, ActivityIndicator } from 'react-native';
-import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
-import { Starship, ApiResponse, fetchSpaceships } from '../../networking/swapi';
+import { View, Text, ListRenderItem } from 'react-native';
+import { Starship, fetchSpaceships } from '../../networking/swapi';
+import InfiniteScrollList from './scrollList';
 
 const StarshipScreen: React.FC = () => {
-    const {
-        data,
-        fetchNextPage,
-        hasNextPage,
-        isFetchingNextPage,
-        status,
-    } = useInfiniteQuery<ApiResponse<Starship>, Error, InfiniteData<ApiResponse<Starship>>, string[], number>({
-        queryKey: ['starships'],
-        queryFn: ({ pageParam }) => fetchSpaceships({ pageParam }),
-        initialPageParam: 1,
-        getNextPageParam: (lastPage) => {
-            if (!lastPage.next) { return undefined; }
-            const nextPageNumber = parseInt(lastPage.next.split('=').pop() || '', 10);
-            return !isNaN(nextPageNumber) ? nextPageNumber : undefined;
-        },
-    });
+    const renderItem: ListRenderItem<Starship> = ({ item }) => (
+        <View style={{ padding: 16 }}>
+            <Text>{item.name}</Text>
+            <Text>Model: {item.model}</Text>
+            <Text>Class: {item.starship_class}</Text>
+        </View>
+    );
 
-    switch (status) {
-        case 'pending':
-            return <ActivityIndicator />;
-        case 'error':
-            return <Text>Error fetching data</Text>;
-        case 'success':
-            return (
-                <FlatList<Starship>
-                    data={data.pages.flatMap(page => page.results)}
-                    keyExtractor={(item) => item.name}
-                    renderItem={({ item }) => (
-                        <View style={{ padding: 16 }}>
-                            <Text>{item.name}</Text>
-                            <Text>Model: {item.model}</Text>
-                            <Text>Class: {item.starship_class}</Text>
-                        </View>
-                    )}
-                    onEndReached={() => {
-                        if (hasNextPage) {
-                            fetchNextPage();
-                        }
-                    }}
-                    onEndReachedThreshold={0.5}
-                    ListFooterComponent={
-                        isFetchingNextPage ? <ActivityIndicator /> : null
-                    }
-                />
-            );
-    }
+    return (
+        <InfiniteScrollList
+            queryKey="starships"
+            fetchFn={fetchSpaceships}
+            renderItem={renderItem}
+        />
+    );
 };
 
 export default StarshipScreen;
